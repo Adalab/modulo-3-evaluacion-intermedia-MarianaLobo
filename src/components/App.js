8f897a5c563f92af303bb317cab99ae602bab40c
Mariana Lobo <m.lobocasas@gmail.com>
2023-03-13 08:49:36 +0100

    add data variable

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,14 @@
 // - Nuestros
 // - Sass
 import '../styles/App.scss';
+import data from '../data/data.json'
+import { useState } from 'react';
 // - Imágenes
 
 /* SECCIÓN DEL COMPONENTE */
 function App() {
   /* VARIABLES ESTADO (DATOS) */
+  const [quotes, setQuotes] = useState(data);
 
   /* EFECTOS (día 5) */
 
@@ -17,7 +20,9 @@ function App() {
   /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
 
   /* HTML */
-  return <div className="App">{/* Aquí va el HTML */}</div>;
+  return <div className="App">
+    <h1>Frases de Friends</h1>
+  </div>;
 }
 
 /* PROP-TYPES */
